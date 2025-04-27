9c864f08edc2cc130885dac3d27c5867dacb0079
wildwinds86 <dclaydon@gmail.com>
2025-04-27 13:58:51 +0100

    added circle

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from constants import *
 def main():
     pygame.init()
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -20,6 +23,8 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        dt = (clock.tick(60) / 1000)
 
 if __name__ == "__main__":
     main()
